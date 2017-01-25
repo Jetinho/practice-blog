d58b7823a0676608eabc9780ebd679ca8b31e324
Julien Jet <jet.julien@gmail.com>
2017-01-25 19:29:44 +0100

    Add post model and controller with routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
