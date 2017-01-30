53bc04bc5f96b6310ac3455881eda4df8e8d6c5c
Julien Jet <jet.julien@gmail.com>
2017-01-30 15:31:37 +0100

    Add pages design, separate home page and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   resources :posts
-  root 'posts#index'
+  root 'posts#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
